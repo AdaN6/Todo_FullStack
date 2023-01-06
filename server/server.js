cd942bd9ed9513e3f1dc33cfac1cddd85066cc15
AdaN6 <a_disney_da@hotmail.com>
2023-01-06 14:45:37 +0100

    installed cors

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
-const port = env.process.PORT || 8080
+
+const port = process.env.PORT || 8080
 
 
 app.listen(8080, () => console.log(`App running at ${port}`));
