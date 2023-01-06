372e06e4c3a3bfcdb11ba515b0f6095e641e59b6
AdaN6 <a_disney_da@hotmail.com>
2023-01-06 14:47:07 +0100

    created the middlewares variable for bodyParser and cors

--- server/server.js
+++ server/server.js
@@ -3,8 +3,14 @@ const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
 
-
 const port = process.env.PORT || 8080
+
+const middlewares = [
+  bodyParser.urlencoded({ extended: false }),
+  bodyParser.json(),
+  cors(),
+];
+app.use(middlewares);
 
 
 app.listen(8080, () => console.log(`App running at ${port}`));
