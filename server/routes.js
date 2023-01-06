f243f1631b867542549d9fed0123cbf607619acd
AdaN6 <a_disney_da@hotmail.com>
2023-01-06 14:56:39 +0100

    installed mongodb

--- server/routes.js
+++ server/routes.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
+const db = require("./database");
+
 router.get("/todos", (req, res) => db.getTodos(res));
 router.post("/complete", (req, res) => db.completeTodo(res, req.body.id));
 
