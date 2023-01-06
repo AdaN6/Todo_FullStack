ce27542534f7636cb8eb08f585da12a186ac6db9
AdaN6 <a_disney_da@hotmail.com>
2023-01-06 14:47:49 +0100

    created a 404 server page

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
 
+
 const port = process.env.PORT || 8080
 
 const middlewares = [
@@ -11,6 +12,11 @@ const middlewares = [
   cors(),
 ];
 app.use(middlewares);
+
+
+
+app.use((req, res, next) => res.status(404).send("Sorry can't find that!"));
+
 
 
 app.listen(8080, () => console.log(`App running at ${port}`));
