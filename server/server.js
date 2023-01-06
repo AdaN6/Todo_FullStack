5a1f68f7d69c9e01e63c774c42aee46606846184
AdaN6 <a_disney_da@hotmail.com>
2023-01-06 15:23:44 +0100

    error middleware 500

--- server/server.js
+++ server/server.js
@@ -18,6 +18,12 @@ app.use("/", routes);
 
 app.use((req, res, next) => res.status(404).send("Sorry can't find that!"));
 
+app.use((err, req, res) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
+
+
 
 
 app.listen(8080, () => console.log(`App running at ${port}`));
